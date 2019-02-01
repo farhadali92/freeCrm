27364739492f3e92169fa7f2d319245d6172b0b3
Farhad Ali <farhad.ali@paaysay.co.uk>
2019-02-01 14:40:57 +0000

    HomePageTest class added

---
+++ src/test/java/com/crm/testcases/HomePageTest.java
@@ -0,0 +1,42 @@
+package com.crm.testcases;
+
+import com.crm.base.BaseTest;
+import com.crm.pages.HomePage;
+import com.crm.pages.LoginPage;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class HomePageTest extends BaseTest {
+
+    private LoginPage loginPage;
+    private HomePage homePage;
+
+
+    HomePageTest() {
+        super();
+    }
+
+    @BeforeMethod
+    public void setup() {
+        initialisation();
+        loginPage = new LoginPage();
+    }
+
+    @Test
+    public void verifyThatUserCanLoginAndCheckHomePageTitleTest() {
+        loginPage.enterUsername(prop.getProperty("username"));
+        loginPage.enterPassword(prop.getProperty("password"));
+        homePage = loginPage.clickLoginBtn();
+        String homePageTitle = homePage.verifyHomePageTitle();
+        Assert.assertEquals("CRMPRO",homePageTitle);
+    }
+
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
+
